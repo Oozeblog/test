5c37f5f31deb07c2370912526996d96b09e817dd
ooze <grigorianna@gmail.com>
2022-10-19 19:39:31 +0400

    Update Revers

--- Revers/Program.cs
+++ Revers/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Написать программу "разворота" числа:
-
+/*
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int number2 = number;
@@ -19,6 +19,19 @@ while(index < digits)
     result = result*10 + number % 10;
     number = number / 10;
     index++;
+}
+Console.WriteLine(result + " осталось от числа " + number );
+*/
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+while(number > 0)
+{
+    result = result*10 + number % 10;
+    number = number / 10;
 }
 
 Console.WriteLine(result);
