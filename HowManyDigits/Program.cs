f939026170a2d1f70a30330c7e9b2fa7bd7788f0
ooze <grigorianna@gmail.com>
2022-10-19 19:38:54 +0400

    Added HowManyDigits

---
+++ HowManyDigits/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа получает от пользователя число, выдаёт количество цифр в числе (работает до 10 знаков).
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digits = 0; 
+
+while (number > 0)
+{
+  digits++;
+  number = (number/10);
+}
+
+Console.WriteLine($"В этом числе {digits} цифр.");
