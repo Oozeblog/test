c1f72775b19c1ad3c402e652ccd638030bad8a82
ooze <grigorianna@gmail.com>
2022-10-21 21:39:08 +0400

    Adde StringOrInt

---
+++ StringOrInt/Program.cs
@@ -0,0 +1,33 @@
+﻿bool DayWeek(int dayNum)
+{
+    if(dayNum == 6 || dayNum == 7)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+Console.WriteLine("Введите цифру соответствующую дню недели(от 1 до 7): ");
+var tostring = Console.ReadLine();
+
+    if (int.TryParse(tostring, out var num))
+    {
+        if(num > 7)
+        {
+            Console.WriteLine ("Эта цифра не соотвтствует дню недели!");
+        }
+        else
+        {
+            bool result = DayWeek(num);
+            Console.Write("Этот день является выходным - ");
+            Console.WriteLine(result);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Некорректный ввод");
+    }
+
