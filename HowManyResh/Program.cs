4d92e26d9be8429405a7f47c2df45cdbb9e90ef4
ooze <grigorianna@gmail.com>
2022-10-29 22:13:35 +0400

    Added  Square Triangle

--- HowManyResh/Program.cs
+++ HowManyResh/Program.cs
@@ -17,7 +17,7 @@ for (int x = 0; x < 13; x++)
 Console.WriteLine($"У уравнения x + y + z = 12 может быть {resh} вариант решений");
 */
 // Найти наибольший общий делитель двух чисел и наименьшее общее кратное
-
+/*
 int GCD(int a, int b)  // Greatest Common Divisor
 {
     int a1;
@@ -51,3 +51,22 @@ int res = GCD(x, y);
 Console.WriteLine($"Наибольшим общим делителем чисел {x} и {y} является {res}");
 int lsm = (x * y) / res;
 Console.WriteLine($"Наименьшим общим кратным чисел {x} и {y} является {lsm}");
+*/
+
+// Вычислить площадь треугольника с тремя заданными сторонами
+
+double SqTr(double a, double b, double c) // формула Герона
+{
+    double p = (a + b + c) / 2;
+    double temp = p * (p - a) * (p - b) * (p - c);
+    p = Math.Sqrt(temp);
+    return p;
+}
+Console.WriteLine("Введите длину первой стороны треугольника: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите длину второй стороны треугольника: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите длину третьей стороны треугольника: ");
+double c = Convert.ToDouble(Console.ReadLine());
+double s = SqTr(a, b, c);
+Console.WriteLine($"Площадь треугольника со сторонами {a}, {b} и {c} равна {Math.Round(s,2)}");
